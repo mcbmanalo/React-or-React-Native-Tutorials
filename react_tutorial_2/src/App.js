cbd3becadaa1ccd482fd17b71d40e9c27cf80057
Carmela Manalo <xtremex471@gmail.com>
2021-04-20 21:19:08 +0800

    Made a functional component that just says Hello! and used it in the App.js

--- react_tutorial_2/src/App.js
+++ react_tutorial_2/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
 
 function App() {
   return (
     <div className="App">
-      
+      <Greet></Greet>
     </div>
   );
 }
