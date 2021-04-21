8e10c0735ffa20823e9c46787d8ad437307f30eb
Carmela Manalo <xtremex471@gmail.com>
2021-04-21 18:52:44 +0800

    Added a new component and started using states

--- react_tutorial_2/src/App.js
+++ react_tutorial_2/src/App.js
@@ -3,18 +3,12 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Carms">
-        <p>This is a children prop</p>
-      </Greet>
-      <Greet name="Maj"></Greet>
-      <Greet name="Marco"></Greet> 
-      <Welcome name="Carms"></Welcome>
-      <Welcome name="Maj"></Welcome>
-      <Welcome name="Marco"></Welcome>
+      <Message></Message>
     </div>
   );
 }
---
+++ react_tutorial_2/src/components/Message.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Message extends Component {
+    constructor() {
+        super()
+        this.state = {
+            message: 'Welcome!'
+        }
+    }
+
+    render() {
+        return <h1>{this.state.message}</h1>
+    }
+}
+
+export default Message; 
