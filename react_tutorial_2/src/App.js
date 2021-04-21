e7b8bdc149c169399c0f8df6298138788fe8dc17
Carmela Manalo <xtremex471@gmail.com>
2021-04-21 19:34:40 +0800

    Added a new class component for learning how to bind eventhandlers so that I can use the this keyword inside the function

--- react_tutorial_2/src/App.js
+++ react_tutorial_2/src/App.js
@@ -7,12 +7,14 @@ import Message from './components/Message'
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
-      <FunctionClick></FunctionClick>
-      <ClassClick/>
+      <EventBind/>
+      {/* <FunctionClick></FunctionClick> */}
+      {/* <ClassClick/> */}
       {/* <Counter></Counter> */}
       {/* <Greet name="Carms" alias="ASTRO fan"/> */}
       {/* <Welcome name="Carms" alias="ASTRO fan"/> */}
