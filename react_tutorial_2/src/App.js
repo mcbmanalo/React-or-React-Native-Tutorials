293998de438dd39707bde86e90c16ce4a67f46f7
Carmela Manalo <xtremex471@gmail.com>
2021-04-21 19:24:34 +0800

    Added an event handler in a new functional component

--- react_tutorial_2/src/App.js
+++ react_tutorial_2/src/App.js
@@ -5,13 +5,15 @@ import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
 
 function App() {
   return (
     <div className="App">
+      <FunctionClick></FunctionClick>
       {/* <Counter></Counter> */}
-      <Greet name="Carms" alias="ASTRO fan"/>
-      <Welcome name="Carms" alias="ASTRO fan"/>
+      {/* <Greet name="Carms" alias="ASTRO fan"/> */}
+      {/* <Welcome name="Carms" alias="ASTRO fan"/> */}
     </div>
   );
 }
