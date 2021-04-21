81923ea242ac3293d11ddc1929dc408009d5131d
Carmela Manalo <xtremex471@gmail.com>
2021-04-21 18:44:33 +0800

    Added a children prop for one Greet component in the App.js and changed the code in the Greet.js to accompany this change

--- react_tutorial_2/src/components/Greet.js
+++ react_tutorial_2/src/components/Greet.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Greet = (props) => {
     console.log(props)
     
-    return <h1>{props.name}</h1>
+    return (
+        <div>
+            <h1>{props.name}</h1>
+            {props.children}
+        </div>
+    )
 }
 
 export default Greet;
