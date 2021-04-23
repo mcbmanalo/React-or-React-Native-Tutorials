7a24561a0c913372852519ccc68510a12980e2ca
Carmela Manalo <xtremex471@gmail.com>
2021-04-23 21:11:01 +0800

    Created a constant for the titles and the paths of the dropdown in the navigation bar

---
+++ react_tutorial_2/src/Hooks/DropdownItems.js
@@ -0,0 +1,19 @@
+export const DropdownItems = [
+    {
+        title:'Title Agent',
+        path:'/title_agent'
+    },
+    {
+        title:'Real Estate Agent, Broker or Team',
+        path:'/real_estate_agent'
+    },
+    {
+        title:'Seller',
+        path:'/seller'
+    },
+    {
+        title:'buyer',
+        path:'/buyer'
+    },
+]
+    
