b5d67429bc27e1f106929ef0e064df197108ebf8
Carmela Manalo <xtremex471@gmail.com>
2021-04-28 21:34:30 +0800

    Added the new prop into each component

--- react_tutorial_2/src/Hooks/pages/AddUser.js
+++ react_tutorial_2/src/Hooks/pages/AddUser.js
@@ -56,20 +56,20 @@ function AddUser() {
 
     return (
         <form onSubmit={handleCreateUser}>
-            <Field fieldTitle="Name" fieldValue={name} onChange={value => setName(value)}></Field>
-            <Field fieldTitle="Username" fieldValue={username} onChange={value => setUsername(value)}></Field>
-            <Field fieldTitle="Email" fieldValue={email} onChange={value => setEmail(value)}></Field>
-            <Field fieldTitle="Street Address" fieldValue={address.street} onChange={value => setAddress({ ...address, street:value})}></Field>
-            <Field fieldTitle="Suite" fieldValue={address.suite} onChange={value => setAddress({ ...address, suite:value})}></Field>
-            <Field fieldTitle="City" fieldValue={address.city} onChange={value => setAddress({ ...address, city:value})}></Field>
-            <Field fieldTitle="Zip Code" fieldValue={address.zipcode} onChange={value => setAddress({ ...address, zipcode:value})}></Field>
-            <Field fieldTitle="Latitude" fieldValue={geoAddress.lat} onChange={value => setGeoAddress({ ...geoAddress, lat:value})}></Field>
-            <Field fieldTitle="Longhitude" fieldValue={geoAddress.lng} onChange={value => setGeoAddress({ ...geoAddress, lng:value})}></Field>
-            <Field fieldTitle="Phone Number" fieldValue={phone} onChange={value => setPhone(value)}></Field>
-            <Field fieldTitle="Website" fieldValue={website} onChange={value => setWebsite(value)}></Field>
-            <Field fieldTitle="Company Name" fieldValue={company.name} onChange={value => setCompany({ ...company, name:value})}></Field>
-            <Field fieldTitle="Company Catchphrase" fieldValue={company.catchPhrase} onChange={value => setCompany({ ...company, catchPhrase:value})}></Field>
-            <Field fieldTitle="Company BS" fieldValue={company.bs} onChange={value => setCompany({ ...company, bs:value})}></Field>
+            <Field fieldType="text" fieldTitle="Name" fieldValue={name} onChange={value => setName(value)}></Field>
+            <Field fieldType="text" fieldTitle="Username" fieldValue={username} onChange={value => setUsername(value)}></Field>
+            <Field fieldType="text" fieldTitle="Email" fieldValue={email} onChange={value => setEmail(value)}></Field>
+            <Field fieldType="text" fieldTitle="Street Address" fieldValue={address.street} onChange={value => setAddress({ ...address, street:value})}></Field>
+            <Field fieldType="text" fieldTitle="Suite" fieldValue={address.suite} onChange={value => setAddress({ ...address, suite:value})}></Field>
+            <Field fieldType="text" fieldTitle="City" fieldValue={address.city} onChange={value => setAddress({ ...address, city:value})}></Field>
+            <Field fieldType="text" fieldTitle="Zip Code" fieldValue={address.zipcode} onChange={value => setAddress({ ...address, zipcode:value})}></Field>
+            <Field fieldType="text" fieldTitle="Latitude" fieldValue={geoAddress.lat} onChange={value => setGeoAddress({ ...geoAddress, lat:value})}></Field>
+            <Field fieldType="text" fieldTitle="Longhitude" fieldValue={geoAddress.lng} onChange={value => setGeoAddress({ ...geoAddress, lng:value})}></Field>
+            <Field fieldType="text" fieldTitle="Phone Number" fieldValue={phone} onChange={value => setPhone(value)}></Field>
+            <Field fieldType="text" fieldTitle="Website" fieldValue={website} onChange={value => setWebsite(value)}></Field>
+            <Field fieldType="text" fieldTitle="Company Name" fieldValue={company.name} onChange={value => setCompany({ ...company, name:value})}></Field>
+            <Field fieldType="text" fieldTitle="Company Catchphrase" fieldValue={company.catchPhrase} onChange={value => setCompany({ ...company, catchPhrase:value})}></Field>
+            <Field fieldType="text" fieldTitle="Company BS" fieldValue={company.bs} onChange={value => setCompany({ ...company, bs:value})}></Field>
             <button>Create User</button>
         </form>
     )
