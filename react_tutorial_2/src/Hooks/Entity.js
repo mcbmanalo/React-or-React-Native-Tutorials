1f642ca7b926419e7937369deff557e319e66e7a
Carmela Manalo <xtremex471@gmail.com>
2021-04-30 21:25:05 +0800

    Just a bridge for the display component. Had trouble with displaying data without using a bridge component. This needs to be further studied so that this component may be deleted or think of another way for this component to be used

--- react_tutorial_2/src/Hooks/Entity.js
+++ react_tutorial_2/src/Hooks/Entity.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Display from './Display'
 
-function Entity() {
+function Entity(props) {
+    // const [entityKeys, setEntityKeys] = useState([])
+    // const entityDefinition = {
+    //     Name: "text",
+    //     Username: "text",
+    //     Email: "text",
+    //     Address: {
+    //         Street: "text",
+    //         Suite: "text",
+    //         City: "text",
+    //         Zipcode: "text",
+    //         Geo: {
+    //             lat: "text",
+    //             lng: "text"
+    //         }
+    //     },
+    //     Phone: 0
+    // }
+
+    // useEffect(() => {
+    //     setEntityKeys(Object.keys(entityDefinition))
+    //     const keys = Object.keys(entityDefinition)
+    //     const values = Object.values(entityDefinition) 
+    //     const len = keys.length
+    //     console.log(keys)
+    //     console.log(values)
+    //     for (var i = 0; i < len ; i++)
+    //     {
+    //         console.log(values[i])
+    //         if(values[i] instanceof Number)
+    //         {
+    //             console.log(`${values[i]} with key of ${keys[i]} is a number`)
+    //         }
+    //         if(values[i] instanceof String)
+    //         {
+    //             console.log(`${values[i]} with key of ${keys[i]} is a string`)
+    //         }
+    //         if(values[i] instanceof Object)
+    //         {
+    //             console.log(`${values[i]} with key of ${keys[i]} is an object`)
+    //         }
+    //     }
+    // }, [])
+
     return (
         <div>
-            Entity Page
+            <Display list={props.entities} toDisplay={props.display_list} displayNames={props.display_list_names}/>
         </div>
     )
 }
