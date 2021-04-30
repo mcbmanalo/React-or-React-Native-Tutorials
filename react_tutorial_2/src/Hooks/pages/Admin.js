4eed5b46332b68820283e007e679de95191457c0
Carmela Manalo <xtremex471@gmail.com>
2021-04-30 21:26:44 +0800

    This component is where all the data will be called and this will call the other components to display the called models. Constants are made for the data that will be displayed

--- react_tutorial_2/src/Hooks/pages/Admin.js
+++ react_tutorial_2/src/Hooks/pages/Admin.js
@@ -2,25 +2,38 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Entity from '../Entity'
+import Display from '../Display'
 
 function Admin() {
     const [users, setUsers] = useState([])
+    const [posts, setPosts] = useState([])
+    const user_display_list_name = ['Name', 'Email', 'Company Name']
+    const user_display_list = ['name', 'email', 'company.name']
+    const post_display_list_name = ['User Id', 'Title', 'Body']
+    const post_display_list = ['userId', 'title', 'body']
     
-    useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/users')
+    const sendGetRequest = async (endpoint, func) => {
+        axios.get(`https://jsonplaceholder.typicode.com/${endpoint}`)
             .then(res => {
                 console.log(res)
-                console.log(...res.data)
-                setUsers(res.data)
+                func(res.data)
             })
             .catch(err => {
                 console.log(err)
             })
+    }
+
+    useEffect(() => {
+        sendGetRequest('users', setUsers);
+        sendGetRequest('posts', setPosts);
     }, [])
 
     return (
         <div>
-            <Entity entities={users}/>
+            {/* <Display list={users} toDisplay={user_display_list} displayNames={user_display_list_name}/> */}
+            <Entity entities={users} display_list={user_display_list} display_list_names={user_display_list_name}/>
+            <Entity entities={posts} display_list={post_display_list} display_list_names={post_display_list_name}/>
+            {/* {users[0].username} */}
         </div>
     )
 }
